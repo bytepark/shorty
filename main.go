34a62e589faac20162c53cbfae16a7dbb1ebf022
Mathias Hertlein <mah@bytepark.de>
2015-03-08 12:53:32 +0100

    Add basic templates for docs, newpost and posts

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 )
 
+type Post struct {
+	Url     string
+	Comment string
+}
+
+var templates = template.Must(template.ParseFiles("html/posts.tmpl.html", "html/newpost.tmpl.html", "html/docs.tmpl.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	err := templates.ExecuteTemplate(w, tmpl, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func handlerListPosts(writer http.ResponseWriter, request *http.Request) {
+	posts := []Post{{Url: "foo", Comment: "Bar"}}
+	renderTemplate(writer, "posts.tmpl.html", posts)
 }
 
 func handlerNewPost(writer http.ResponseWriter, request *http.Request) {
+	renderTemplate(writer, "newpost.tmpl.html", nil)
 }
 
 func handlerDocs(writer http.ResponseWriter, request *http.Request) {
+	renderTemplate(writer, "docs.tmpl.html", nil)
 }
 
 func handlerShortLink(writer http.ResponseWriter, request *http.Request) {
