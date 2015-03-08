05ff994fa706a81cbcb71ee2c1f01ff24c4e9060
fbatschi <fbatschi@users.noreply.github.com>
2015-03-08 16:10:20 +0100

    add initial usage of sqlite. implement adding of new urls and retrieval of posts

--- main.go
+++ main.go
@@ -1,47 +1,48 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
     "github.com/shaoshing/train"
+    "html/template"
+    "net/http"
+    "fmt"
+    posts "github.com/bytepark/shorty/posts"
 )
-
-type Post struct {
-	Url     string
-	Comment string
-}
 
 var templates = template.Must(template.ParseFiles("html/posts.tmpl.html", "html/newpost.tmpl.html", "html/docs.tmpl.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-	err := templates.ExecuteTemplate(w, tmpl, data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+    err := templates.ExecuteTemplate(w, tmpl, data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
 
 func handlerListPosts(writer http.ResponseWriter, request *http.Request) {
-	posts := []Post{{Url: "foo", Comment: "Bar"}}
-	renderTemplate(writer, "posts.tmpl.html", posts)
+    myposts := posts.ListPosts()
+    renderTemplate(writer, "posts.tmpl.html", myposts)
 }
 
 func handlerNewPost(writer http.ResponseWriter, request *http.Request) {
-	renderTemplate(writer, "newpost.tmpl.html", nil)
+    url := request.FormValue("url")
+    fmt.Println("new url", url)
+
+    mypost := posts.NewPost(url, "testlink")
+    renderTemplate(writer, "newpost.tmpl.html", mypost)
 }
 
 func handlerDocs(writer http.ResponseWriter, request *http.Request) {
-	renderTemplate(writer, "docs.tmpl.html", nil)
+    renderTemplate(writer, "docs.tmpl.html", nil)
 }
 
 func handlerShortLink(writer http.ResponseWriter, request *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/posts", handlerListPosts)
-	http.HandleFunc("/newpost", handlerNewPost)
-	http.HandleFunc("/docs", handlerDocs)
-	http.HandleFunc("/(pattern)", handlerShortLink)
-	http.HandleFunc("/", handlerListPosts)
+    http.HandleFunc("/posts", handlerListPosts)
+    http.HandleFunc("/newpost", handlerNewPost)
+    http.HandleFunc("/docs", handlerDocs)
+    http.HandleFunc("/(pattern)", handlerShortLink)
+    http.HandleFunc("/", handlerListPosts)
     train.ConfigureHttpHandler(nil)
-	http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":8080", nil)
 }
